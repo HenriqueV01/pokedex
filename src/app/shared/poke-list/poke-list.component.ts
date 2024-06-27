82041ade4260b3f65eb54f53f2fa8d4b74358ff9
HenriqueV01 <henrique.unibratec01@gmail.com>
2024-06-27 19:49:51 -0300

    Add: poke-api.service

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from '../../service/poke-api.service';
 
 @Component({
   selector: 'app-poke-list',
   templateUrl: './poke-list.component.html',
   styleUrl: './poke-list.component.scss'
 })
-export class PokeListComponent {
+export class PokeListComponent implements OnInit{
+
+  constructor(private pokeApiService: PokeApiService){}
+
+  ngOnInit(): void {
+      this.pokeApiService.apiListAllPokemons.subscribe(
+        res => console.log(res)
+      );
+  }
 
 }
